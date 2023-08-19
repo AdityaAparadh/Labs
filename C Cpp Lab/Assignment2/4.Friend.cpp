4f8d2b0bf63c69ed98c74e18d8a69d48582afde7
Aditya Aparadh <aditya.aparadh.0@gmail.com>
2023-08-19 21:44:23 +0530

    Did Friend Function Assignment

---
+++ C Cpp Lab/Assignment2/4.Friend.cpp
@@ -0,0 +1,46 @@
+/*
+Aditya Aparadh
+PRN: 22610082           SYIT                S3
+Program to add two complex numbers using Friend Function
+*/
+
+#include <iostream>
+using namespace std;
+
+class Complex;
+Complex Add(Complex, Complex);
+
+
+class Complex{
+    private:
+        float real;
+        float imaginary;
+
+    public:
+        Complex(float r, float i){
+            real = r;
+            imaginary = i;
+        } 
+        void Print(){
+            cout << real << " + " << imaginary << "i" << endl;
+        }
+
+
+    friend Complex Add(Complex,Complex);        //Friend Function
+
+};
+
+
+Complex Add(Complex a, Complex b){
+    return Complex(a.real + b.real, a.imaginary + b.imaginary);
+}
+
+
+
+int main(){
+    Complex a(1, 2);
+    Complex b(3, 4);
+    Complex c = Add(a, b);
+    c.Print();
+    return 0;
+}
